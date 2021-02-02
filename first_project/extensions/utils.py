e419028498c99ca325bf5b898443568f4db56aa3
xandner <karimixander@gmail.com>
2021-02-02 18:45:56 +0330

    change enghish number to persian number

--- first_project/extensions/utils.py
+++ first_project/extensions/utils.py
@@ -1,6 +1,26 @@
 from . import jalali
 from django.utils import timezone
 
+def persian_numer_converter(mystr):
+    numbers={
+        "0":"۰",
+        "1":"۱",
+        "2":"۲",
+        "3":"۳",
+        "4":"۴",
+        "5":"۵",
+        "6":"۶",
+        "7":"۷",
+        "8":"۸",
+        "9":"۹",
+        
+    }
+    for e,p in numbers.items():
+        # .items ==> e is key , p is value
+        mystr=mystr.replace(e,p)
+    return mystr
+
+
 
 def jalali_converter(time):
     j_month=['فروردین','اردیبهشت','خرداد','تیر','مرداد','شهریور','مهر','آبان','آذر','دی','بهمن','اسفند']
@@ -14,4 +34,4 @@ def jalali_converter(time):
             time_list[1]=month
             break
     output = f'{time_list[0]}/{time_list[1]}/{time_list[2]}. ساعت{time.hour}:{time.minute}'
-    return output
+    return persian_numer_converter(output)
